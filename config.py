b8d78153687190a5d53a565941bb0dcbe6d68871
Franko <dajafranko@gmail.com>
2025-01-11 00:10:09 +0100

    Initial commit for MG Project backend

---
+++ config.py
@@ -0,0 +1,7 @@
+DATABASE_CONFIG = {
+    'host': 'localhost',        # Database server address
+    'database': 'mg_project',  # Name of your PostgreSQL database
+    'user': 'postgres',        # Your PostgreSQL username
+    'password': 'Daja2409', # Your PostgreSQL password
+    'port': 5432               # Default PostgreSQL port
+}
