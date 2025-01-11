2fa82a4f104c43ecd70e792ebce865370d75372d
Franko <dajafranko@gmail.com>
2025-01-11 01:36:05 +0100

    change table name from user to users

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ db = SQLAlchemy(app)
 
 # Models
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     username = db.Column(db.String(100), unique=True, nullable=False)
